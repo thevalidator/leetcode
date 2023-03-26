165465db5051d00191d99d3fb541262a244a43de
thevalidator <the.validator@yandex.ru>
2023-03-26 22:07:46 +0300

    Problem 876 template added

---
+++ src/main/java/ru/thevalidator/leetcode/problems/p00876/ListNode.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2023 thevalidator
+ */
+package ru.thevalidator.leetcode.problems.p00876;
+
+/**
+ * @author thevalidator <the.validator@yandex.ru>
+ */
+public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ src/main/java/ru/thevalidator/leetcode/problems/p00876/Solution.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2023 thevalidator
+ */
+package ru.thevalidator.leetcode.problems.p00876;
+
+/**
+ * @author thevalidator <the.validator@yandex.ru>
+ */
+public class Solution {
+
+    public ListNode middleNode(ListNode head) {
+
+    }
+}

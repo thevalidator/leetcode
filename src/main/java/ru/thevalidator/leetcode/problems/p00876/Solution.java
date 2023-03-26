dd879d0b7ec4e8481ec0e60efb2d2bc227305181
thevalidator <the.validator@yandex.ru>
2023-03-26 22:28:24 +0300

    Problem 876 solved

--- src/main/java/ru/thevalidator/leetcode/problems/p00876/Solution.java
+++ src/main/java/ru/thevalidator/leetcode/problems/p00876/Solution.java
@@ -9,6 +9,23 @@ package ru.thevalidator.leetcode.problems.p00876;
 public class Solution {
 
     public ListNode middleNode(ListNode head) {
-
+        if (head.next == null) {
+            return head;
+        }
+        
+        ListNode middle = head;
+        ListNode actual = head;
+        int counter = 0;
+        int middleIndex = 1;
+        while (actual != null) {
+            counter++;
+            if (counter >= middleIndex * 2) {
+                middleIndex = counter / 2 + 1;
+                middle = middle.next;
+            }
+            actual = actual.next;
+        }
+        
+        return middle;
     }
 }

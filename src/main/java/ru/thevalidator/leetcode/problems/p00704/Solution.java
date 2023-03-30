e4486f9b40db39d028ebb76804a538fbe6bcabde
thevalidator <the.validator@yandex.ru>
2023-03-30 18:37:30 +0300

    Problem 704 solved

---
+++ src/main/java/ru/thevalidator/leetcode/problems/p00704/Solution.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2023 thevalidator
+ */
+package ru.thevalidator.leetcode.problems.p00704;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author thevalidator <the.validator@yandex.ru>
+ */
+public class Solution {
+    
+    public static void main(String[] args) throws InterruptedException {
+        //System.out.println(search(new int[] {-1,0,3,5,9,12}, 9));
+        System.out.println(search(new int[] {5}, 5));
+    }
+
+    public static int search(int[] nums, int target) throws InterruptedException {
+        
+        int ans = -1;
+        
+        if (nums.length == 1) {
+            if (nums[0] == target) {
+                return 0;
+            } else {
+                return ans;
+            }
+        }
+        
+        
+        int left = 0;
+        int right = nums.length - 1;
+        
+        
+        
+        
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            System.out.printf(">> left=%d\tmiddle=%d\tright=%s\n", left, middle, right);
+            
+            if (nums[middle] == target) {
+                return middle;
+            } else if (target < nums[middle]) {
+                right = middle - 1;
+                //middle -= (right - left + 1) / 2;
+            } else if (target > nums[middle]) {
+                left = middle + 1;
+                //middle += (right - left + 1) / 2;
+            }
+            System.out.printf("<< left=%d\tmiddle=%d\tright=%s\n\n", left, middle, right);
+            
+            TimeUnit.SECONDS.sleep(3);
+        }
+        
+        
+        return ans;
+    }
+}

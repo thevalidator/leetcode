8739f52f8854d5fe5d3baf15e43a016ca3bc901d
thevalidator <the.validator@yandex.ru>
2023-04-06 23:00:43 +0300

    added second solution

--- src/main/java/ru/thevalidator/leetcode/problems/p00299/Solution.java
+++ src/main/java/ru/thevalidator/leetcode/problems/p00299/Solution.java
@@ -12,32 +12,32 @@ import java.util.Map;
 public class Solution {
 
     public static String getHint(String secret, String guess) {
-        
+
         int bulls = 0;
         int cows = 0;
         Map<Character, Integer> map = new HashMap<>();
         Map<Character, Integer> map2 = new HashMap<>();
         for (int i = 0; i < secret.length(); i++) {
-            
+
             if (secret.charAt(i) == guess.charAt(i)) {
                 bulls++;
                 continue;
             }
-            
+
             if (map.containsKey(secret.charAt(i))) {
                 map.put(secret.charAt(i), (map.get(secret.charAt(i)) + 1));
             } else {
                 map.put(secret.charAt(i), 1);
             }
-            
+
             if (map2.containsKey(guess.charAt(i))) {
                 map2.put(guess.charAt(i), (map2.get(guess.charAt(i)) + 1));
             } else {
                 map2.put(guess.charAt(i), 1);
             }
-            
+
         }
-        
+
         for (Map.Entry<Character, Integer> entry: map2.entrySet()) {
             Character key = entry.getKey();
             Integer value = entry.getValue();
@@ -50,10 +50,29 @@ public class Solution {
                 }
             }
         }
-        
+
         return String.format("%dA%dB", bulls, cows);
     }
-    
+
+    public static String getHint2(String secret, String guess) {
+        int bulls = 0;
+        int cows = 0;
+        int[] numbers = new int[10];
+        for (int i = 0; i < secret.length(); i++) {
+            if (secret.charAt(i) == guess.charAt(i)) {
+                bulls++;
+            } else {
+                if (numbers[secret.charAt(i) - '0']++ < 0) {
+                    cows++;
+                }
+                if (numbers[guess.charAt(i) - '0']-- > 0) {
+                    cows++;
+                }
+            }
+        }
+        return String.format("%dA%dB", bulls, cows);
+    }
+
     public static void main(String[] args) {
         System.out.println(getHint("1807", "7810") + " //1A3B ");
         System.out.println(getHint("1123", "0111") + " //1A1B ");
